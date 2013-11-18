a49299b58ea62c3b3e654581348f0387465d1714
Travis McWilliam <travis@sproutsocial.com>
2013-11-18 16:39:15 -0600

    updates

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -4,7 +4,12 @@ var Lateral = (function() {
     aspectRatio: getAspectRatio(),
 
     init: function() {
+      // jQuery.support.cors = true;
       lateral.orbit();
+      $('#testAPIButton').click(function(ev){
+        // lateral.testAPI();
+        lateral.testWineAPI();
+      });
     },
 
     orbit: function() {
@@ -15,6 +20,66 @@ var Lateral = (function() {
         fluid: true,
         captions: true,
         bullets: true,
+      });
+    },
+
+    testAPI: function() {
+      var wsUrl = "https://api.mindbodyonline.com/0_5/SiteService.asmx";
+      var soapRequest = '<soapenv:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' +
+                        '<soapenv:Header/>' +
+                        '<soapenv:Body>' +
+                        '<GetClassDescriptions xmlns="http://clients.mindbodyonline.com/api/0_5">' +
+                        '<Request>' +
+                        '<SourceCredentials>' +
+                          '<SourceName>LateralFitness</SourceName>' +
+                          '<Password>Lateral41</Password>' +
+                          '<SiteIDs>' +
+                            '<int>33786</int>' +
+                          '</SiteIDs>' +
+                        '</SourceCredentials>' +
+                        '<XMLDetail>Full</XMLDetail>' +
+                        '<PageSize>10</PageSize>' +
+                        '<CurrentPageIndex>0</CurrentPageIndex>' +
+                        '<ProgramIDs>' +
+                          '<int>71</int>' +
+                        '</ProgramIDs>' +
+                        '</Request>' +
+                        '</GetClassDescriptions>' +
+                        '</soapenv:Body>' +
+                        '</soapenv:Envelope>';
+
+
+      $.ajax({
+        type: "POST",
+        url: wsUrl,
+        contentType: "text/xml",
+        dataType: "xml",
+        data: soapRequest,
+        crossDomain: true,
+        success: function(data, textStatus, xhr){
+          console.log(data);
+          console.log(textStatus);
+          console.log(xhr);
+        },
+        error: function(xhr, textStatus, errorThrown) {
+          console.log(xhr);
+          console.log(textStatus);
+          console.log(errorThrown);
+        }
+      });
+    },
+
+    testWineAPI: function() {
+      $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/wines",
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(xhr, error) {
+          console.log(xhr);
+          console.log(error);
+        }
       });
     }
   };
